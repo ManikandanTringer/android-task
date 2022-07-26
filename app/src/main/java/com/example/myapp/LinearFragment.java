0500114e896ec810eba5dd587fadc50308b3d899
ManikandanTringer <manikandan.r@tringapps.com>
2022-07-26 18:11:11 +0530

    constraint-its-attributes

--- app/src/main/java/com/example/myapp/LinearFragment.java
+++ app/src/main/java/com/example/myapp/LinearFragment.java
@@ -1,5 +1,7 @@
 package com.example.myapp;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LinearFragment extends Fragment {
+
+    private TextView textView1;
+    View view;
+    LinearLayout linearLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,12 +26,52 @@ public class LinearFragment extends Fragment {
 
     }
 
+    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
 
-        return inflater.inflate(R.layout.fragment_linear, container, false);
+        view = inflater.inflate(R.layout.fragment_linear, container, false);
+        textView1=view.findViewById(R.id.textview1);
+        linearLayout=view.findViewById(R.id.linear_layout);
+        //Layout params
+        if(linearLayout.getOrientation() != LinearLayout.VERTICAL) {
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+        }
+        else{
+            Toast.makeText(getContext(),"Orientation is already vertical",Toast.LENGTH_LONG);
+        }
+        textView1.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+
+        ));
+        textView1.setEms(2);
+
+//        linearLayout.addView(textView1);
+
+        //Create a button in java file
+        Button btn = new Button(getContext());
+        btn.setText("Welcome");
+        btn.setBackgroundColor(Color.parseColor("#FCFCFC"));
+//        btn.setTextColor(Color.parseColor("#FCFCFC"));
+        btn.setId(10101);
+        btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//        btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 150));
+//        btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 50));
+        linearLayout.addView(btn);
+        Button btn1=view.findViewById(10101);
+         btn1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Toast.makeText(getContext(),"Welcoming You",Toast.LENGTH_LONG).show();
+             }
+         });
+
+
+
+        return view;
 
     }
 }
