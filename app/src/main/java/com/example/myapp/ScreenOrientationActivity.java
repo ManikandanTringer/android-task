38476d7c8fec698c8879d2f53358f11ca21cd7d7
ManikandanTringer <manikandan.r@tringapps.com>
2022-08-17 21:49:59 +0530

    Volley and screen orientation

---
+++ app/src/main/java/com/example/myapp/ScreenOrientationActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenOrientationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_orientation);
+    }
+}
