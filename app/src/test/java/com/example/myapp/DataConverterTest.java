81231af811b189bcd5b9913a94d2ea54fd4705c5
ManikandanTringer <manikandan.r@tringapps.com>
2022-09-06 18:49:13 +0530

    MVVM viewBinding dataBinding

---
+++ app/src/test/java/com/example/myapp/DataConverterTest.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import junit.framework.TestCase;
+
+public class DataConverterTest extends TestCase {
+
+    public void testConvertImageToByteArray() {
+    }
+
+    public void testConvertByteArrayToBitmap() {
+    }
+}
